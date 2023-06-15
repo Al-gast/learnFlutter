763870edcf71eb59dfc9d1e54f7a72d716ad00e3
Ata <agastyo2004@gmail.com>
2023-06-15 13:54:01 +0700

    list view

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // basic widget
-
+    // 01 basic widget
     // var basic_widget = const Center(
     //   child: Padding(
     //     padding: EdgeInsets.all(8.0),
@@ -23,39 +22,80 @@ class MyApp extends StatelessWidget {
     //   ),
     // );
 
-    // Visible and invisible widget
+    // 02 Visible and invisible widget
+    // var visible_invisible_wiget = Stack(
+    //   children: [
+    //     Container(
+    //       height: 400,
+    //       width: 400,
+    //       color: Colors.greenAccent,
+    //     ),
+    //     Container(
+    //       height: 300,
+    //       width: 300,
+    //       color: Colors.lime,
+    //     ),
+    //     Container(
+    //       height: 200,
+    //       width: 200,
+    //       color: Colors.lightBlue,
+    //     ),
+    //     Container(
+    //       height: 100,
+    //       width: 100,
+    //       color: Colors.blueGrey,
+    //     ),
+    //   ],
+    // );
 
-    var visible_invisible_wiget = Stack(
-      children: [
-        Container(
-          height: 400,
-          width: 400,
-          color: Colors.greenAccent,
+    // 03 List View
+    final List<Color> myColor = [
+      Colors.limeAccent,
+      Colors.lightBlue,
+      Colors.black,
+      Colors.greenAccent
+    ];
+
+    final List<Widget> myList = List.generate(
+      100,
+      (index) => Text(
+        "${index + 1}",
+        style: TextStyle(
+          fontSize: 20 +
+              double.parse(
+                index.toString(),
+              ),
         ),
-        Container(
-          height: 300,
-          width: 300,
-          color: Colors.lime,
-        ),
-        Container(
-          height: 200,
-          width: 200,
-          color: Colors.lightBlue,
-        ),
-        Container(
-          height: 100,
-          width: 100,
-          color: Colors.blueGrey,
-        ),
-      ],
+      ),
     );
+
+    var list_view = ListView.separated(
+      separatorBuilder: (context, index) {
+        return Container(
+          height: 50,
+          color: Colors.cyan,
+        );
+      },
+      itemCount: 4,
+      itemBuilder: (context, index) {
+        return Container(
+          height: 300,
+          color: myColor[index],
+        );
+      },
+    );
+
+    var list_view_generate = ListView(
+      children: myList,
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text("May Apps"),
         ),
-        body: visible_invisible_wiget,
+        body: list_view_generate,
       ),
     );
   }
